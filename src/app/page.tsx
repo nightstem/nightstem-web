3b71649439ccbff7e99efc919e3c9cec2b4cd962
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2025-08-02 20:35:39 -0600

    Add color palette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-center">
-        <img
-          src={"/logo.webp"}
-          alt="logo"
-          width={60}
-          height={60}
-          className="animate-pulse"
-        />
-        <h1 className="text-lg">This page is under construction</h1>
-      </main>
-    </div>
+    <main className="w-dvw h-dvh grid place-content-center sm:items-center">
+      <article className="flex flex-col gap-7">
+        <h1 className="heading-lg flex flex-col text-center leading-none">
+          <img
+            src={"/logo.webp"}
+            alt="logo"
+            width={32}
+            height={32}
+            className="self-end mr-6 animate-pulse"
+          />
+          <span>Nightstem</span>
+        </h1>
+
+        <span className="text-neutral-500">
+          This page is under construction
+        </span>
+      </article>
+    </main>
   );
 }
