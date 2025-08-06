903c8e4557be8371e7579a9ccdd857a750c7e62e
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2025-08-06 00:22:10 -0600

    Small tweak, version visible now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
 export default function Home() {
+  const version = process.env.NEXT_PUBLIC_APP_VERSION;
+
   return (
-    <main className="w-dvw h-dvh grid place-content-center sm:items-center">
-      <article className="flex flex-col gap-7">
-        <h1 className="flex flex-col text-center">
+    <main className="relative w-dvw h-dvh bg-black overflow-hidden text-white grid place-content-center sm:items-center">
+      <article className="relative flex flex-col gap-5 items-center text-center">
+        <header className="flex flex-col text-center">
           <img
             src={'/logo.webp'}
             alt="logo"
             width={32}
             height={32}
-            className="self-end mr-6 animate-pulse"
+            className="self-end animate-pulse"
           />
-          <span className="heading-lg">Nightstem</span>
-        </h1>
+          <h1 className="heading-lg">Nightstem</h1>
+        </header>
 
-        <span className="text-neutral-500">
+        <span className="text-neutral-500 text-sm tracking-wide">
           This page is under construction
         </span>
       </article>
+
+      <span className="absolute bottom-4 right-4 text-xs text-neutral-400 opacity-60 font-mono">
+        v{version}
+      </span>
     </main>
   );
 }
