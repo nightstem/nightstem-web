f3e3c054b1f208fa9847abebbe64d18e533ad981
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2025-08-12 20:38:53 -0600

    Add hashIndex util function

---
+++ src/utils/hashIndex/hashIndex.ts
@@ -0,0 +1,9 @@
+export const hashIndex = (seed: string, max: number) => {
+  if (!seed || max <= 1) return 0;
+
+  let h = 5381;
+  for (let i = 0; i < seed.length; i++) h = (h * 33) ^ seed.charCodeAt(i);
+  return Math.abs(h) % max;
+};
+
+export default hashIndex;
---
+++ src/utils/hashIndex/index.ts
@@ -0,0 +1,2 @@
+export * from './hashIndex';
+export { default } from './hashIndex';
