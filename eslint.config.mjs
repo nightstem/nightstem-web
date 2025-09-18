c9f9adec879e0eab5f08c549cd94cc4207698b32
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2025-09-18 00:58:45 -0600

    Format eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook';
+import { defineConfig } from 'eslint/config';
+
 import js from '@eslint/js';
 import security from 'eslint-plugin-security';
+import storybook from 'eslint-plugin-storybook';
 import { importX } from 'eslint-plugin-import-x';
 import reactPerfPlugin from 'eslint-plugin-react-perf';
 
@@ -11,69 +13,71 @@ import { FlatCompat } from '@eslint/eslintrc';
 
 import vitest from '@vitest/eslint-plugin';
 
-const __filename = fileURLToPath(import.meta.url),
-  __dirname = dirname(__filename),
-  compat = new FlatCompat({
-    baseDirectory: __dirname,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = defineConfig([
+  {
+    ignores: [
+      'node_modules/**',
+      'coverage/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      '**/*.d.ts',
+    ],
+  },
+  js.configs.recommended,
+  security.configs.recommended,
+  importX.flatConfigs.recommended,
+  importX.flatConfigs.typescript,
+  reactPerfPlugin.configs.flat.all,
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      'no-console': 'error',
+      '@next/next/no-img-element': 'off',
+      'security/detect-object-injection': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
   }),
-  eslintConfig = [
-    {
-      ignores: [
-        'node_modules/**',
-        'coverage/**',
-        '.next/**',
-        'out/**',
-        'build/**',
-        '**/*.d.ts',
-      ],
+  {
+    files: ['.storybook/**'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
     },
-    js.configs.recommended,
-    security.configs.recommended,
-    importX.flatConfigs.recommended,
-    importX.flatConfigs.typescript,
-    reactPerfPlugin.configs.flat.all,
-    ...compat.config({
-      extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
-      rules: {
-        'no-console': 'error',
-        '@next/next/no-img-element': 'off',
-        'security/detect-object-injection': 'off',
-        '@typescript-eslint/no-explicit-any': 'error',
+  },
+  ...storybook.configs['flat/recommended'],
+  {
+    files: ['**/*.{test,spec}.{ts,tsx}'],
+    plugins: {
+      vitest,
+    },
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals,
       },
-    }),
-    {
-      files: ['.storybook/**'],
-      rules: {
-        '@typescript-eslint/no-require-imports': 'off',
+      parserOptions: {
+        project: true,
       },
     },
-    ...storybook.configs['flat/recommended'],
-    {
-      files: ['**/*.{test,spec}.{ts,tsx}'],
-      plugins: {
-        vitest,
-      },
-      languageOptions: {
-        globals: {
-          ...vitest.environments.env.globals,
-        },
-        parserOptions: {
-          project: true,
-        },
-      },
-      settings: {
-        vitest: {
-          typecheck: true,
-        },
-      },
-      rules: {
-        ...vitest.configs.all.rules,
-        'vitest/no-hooks': 'off',
-        'vitest/prefer-lowercase-title': 'off',
-        'vitest/prefer-expect-assertions': 'off',
-        'vitest/require-mock-type-parameters': 'off',
+    settings: {
+      vitest: {
+        typecheck: true,
       },
     },
-  ];
+    rules: {
+      ...vitest.configs.all.rules,
+      'vitest/no-hooks': 'off',
+      'vitest/prefer-lowercase-title': 'off',
+      'vitest/prefer-expect-assertions': 'off',
+      'vitest/require-mock-type-parameters': 'off',
+    },
+  },
+]);
 
 export default eslintConfig;
